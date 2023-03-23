4e30c0957fb2cdb2afb747a5c635053d384ec837
Shaun MacWilliam <shaun.macwilliam@gmail.com>
2023-03-23 21:40:40 +0000

    eslint config prettier set up

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,7 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: "eslint:recommended",
+  extends: ["eslint:recommended", "prettier"],
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
@@ -12,6 +12,5 @@ module.exports = {
   },
   rules: {
     "no-console": "off",
-    quotes: ["error", "double"],
   },
 };
