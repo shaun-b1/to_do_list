6eba124432352f6ca4a41939039617f4095a8c24
Shaun MacWilliam <shaun.macwilliam@gmail.com>
2023-06-24 17:33:36 +0100

    Error handling added for attempting to add a todo without a project selected

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,5 @@
 import { toDoModal } from "./todo_modal";
+import { projectManager } from "./project_manager";
 export { main, footer };
 
 function main() {
@@ -13,8 +14,13 @@ function main() {
   button.innerHTML =
     `<span class="material-icons-round"></span>` + "<p>Add todo</p>";
   button.addEventListener("click", () => {
-    const dialog = document.body.appendChild(toDoModal());
-    dialog.showModal();
+    if (projectManager.getCurrentProject() == null) {
+      const dialog = document.body.appendChild(warning());
+      dialog.showModal();
+    } else {
+      const dialog = document.body.appendChild(toDoModal());
+      dialog.showModal();
+    }
   });
 
   const todos = document.createElement("ul");
@@ -32,4 +38,21 @@ function footer() {
   footer.innerHTML = `<p>Made by <a href="https://github.com/shaun-b1">Shaun MacWilliam</a> in &#x1F1EC;&#x1F1E7;</p>`;
 
   return footer;
+}
+
+function warning() {
+  const warningBox = document.createElement("dialog");
+  warningBox.classList.add("warning-box");
+
+  const text = document.createElement("p");
+  text.textContent = "You need to select a Project to add a Todo to";
+
+  const button = document.createElement("button");
+  button.textContent = "Okay!";
+  button.addEventListener("click", () => {
+    warningBox.remove();
+  });
+
+  warningBox.append(text, button);
+  return warningBox;
 }
